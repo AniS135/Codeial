53aac9638baa71cceb8fb152539ef2fe66fe4a21
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-06-21 17:42:27 +0530

    Some more routes and controlles

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ console.log("Router loaded");
 
 
 router.get('/',homeController.home);
+router.get('/about',homeController.about);
+router.use('/users',require('./users'));
+
+//for any further routes access from here
+// router.use('/routerName',require('./routerFile'));
 
 
 module.exports=router;
