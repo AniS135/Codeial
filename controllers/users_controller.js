5eb5080148dc4b1da3a964028c26f57acb2fdfa6
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-06-23 23:27:50 +0530

    Getting details from user to Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
     res.render('profile',{
         title:'UserName'
@@ -20,7 +22,24 @@ module.exports.signIn=function(req,res){
 
 // get the sign up data
 module.exports.create=function(req,res){
-    // TODO Later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email}, function(err,user){
+        if(err){
+            console.log('Error in finding user in sign up');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('Error in creating user while signing up'); return;}
+
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            res.redirect('back');
+        }
+    })
 }
 
 module.exports.createSession =function(req,res){
