d40889e25a090039de0e4cbd5a6e55ed2824721e
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-06-25 19:52:57 +0530

    Using passport to authenticate

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const expressLayouts=require('express-ejs-layouts');
 const db= require('./config/mongoose');
 const { urlencoded } = require("express");
 
+// extract style and scripts from sub pages into the layout
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
 const app=express();
 
 const port=8000;
@@ -20,12 +25,28 @@ app.use(express.static(__dirname + '/assets'));
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
-// use express router
-app.use('/',require('./routes'));
+
 
 // set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name:'codeial',
+    // TODO change the secret before the deployment in production mode
+    secret:"blahsomething",
+    saveUninitialized: false,
+    resave:false,
+    cookie:{
+        maxAge:(1000*60*100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/',require('./routes'));
 
 
 app.listen(port,function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const expressLayouts=require('express-ejs-layouts');
+const passport=require('passport');
 
 const router=express.Router();
 const usersController= require('../controllers/users_controller');
@@ -12,6 +13,12 @@ router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 
 router.post('/create',usersController.create);
+
+// Use passport as middleware to authenticate
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
+),usersController.createSession);
 
 
 module.exports=router;
